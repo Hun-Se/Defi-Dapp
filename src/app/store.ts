76285fb2ab60b0a83d7a97ca593a4bb9882d8794
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2023-03-24 23:25:16 +0900

    first commit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import connectWalletReducer from "../features/connect_wallet/connectSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    connect: connectWalletReducer,
   },
 });
 
---
+++ truffle-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  contracts_directory: "./src/contracts",
+  contracts_build_directory: "./src/truffle_abis",
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*", // Match any network id
+    },
+  },
+
+  compilers: {
+    solc: {
+      version: "^0.8.0",
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
