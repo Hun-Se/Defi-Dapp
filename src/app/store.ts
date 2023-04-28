c72edb5b7b3d8d357b9a50203cc799cf39cc73a9
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2023-04-28 23:11:29 +0900

    필요없는 파일 삭제

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
-import connectWalletReducer from "../features/connect_wallet/connectSlice";
+import connectWalletReducer from "../redux/wallet/connectWalletSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     connect: connectWalletReducer,
   },
 });
